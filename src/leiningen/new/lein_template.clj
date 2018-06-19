572c8d4030055340207e1837b1b521c0bf672323
David Banks <amoebae@gmail.com>
2018-06-19 11:50:20 +0100

    demo literal file inclusion

--- src/leiningen/new/lein_template.clj
+++ src/leiningen/new/lein_template.clj
@@ -1,14 +1,20 @@
-(ns leiningen.new.lein-template
+1(ns leiningen.new.lein-template
   (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
             [leiningen.core.main :as main]))
 
-(def render (renderer "lein-template"))
+;; Using a nil renderer will just slurp the file.
+;; This is questionable API design perhaps, so we conceal it here.
 
-(defn lein-template
-  "FIXME: write documentation"
-  [name]
+(def expand-mustache (renderer "lein-template"))
+(def slurp-file (fn [path]
+                  (apply (renderer "lein-template")
+                         [path nil])))
+
+
+(defn lein-template [name]
   (let [data {:name name
               :sanitized (name-to-path name)}]
     (main/info "Generating fresh 'lein new' lein-template project.")
     (->files data
-             ["src/{{sanitized}}/foo.clj" (render "foo.clj" data)])))
+             ["src/{{sanitized}}/foo.clj" (expand-mustache "foo.clj" data)]
+             ["literal_file.dat" (slurp-file "literal_file.dat")])))
