6f33c3093fa5ac8c26fc1b91a119318fb1127e11
David Banks <amoebae@gmail.com>
2018-06-19 11:54:23 +0100

    adding leiningen project file

--- src/leiningen/new/lein_template.clj
+++ src/leiningen/new/lein_template.clj
@@ -1,5 +1,5 @@
 1(ns leiningen.new.lein-template
-  (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
+  (:require [leiningen.new.templates :refer [renderer name-to-path ->files project-name multi-segment sanitize-ns]]
             [leiningen.core.main :as main]))
 
 ;; Using a nil renderer will just slurp the file.
@@ -12,9 +12,13 @@
 
 
 (defn lein-template [name]
-  (let [data {:name name
+  (let [main-ns (multi-segment (sanitize-ns name))
+        data {:raw-name name
+              :name (project-name name)
+              :namespace main-ns
               :sanitized (name-to-path name)}]
     (main/info "Generating fresh 'lein new' lein-template project.")
     (->files data
              ["src/{{sanitized}}/foo.clj" (expand-mustache "foo.clj" data)]
+             ["project.clj" (expand-mustache "project.clj" data)]
              ["literal_file.dat" (slurp-file "literal_file.dat")])))
