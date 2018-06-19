28e79339959a1de595f4c6c0c441c4cbc6695c1f
David Banks <amoebae@gmail.com>
2018-06-19 16:13:46 +0100

    fixing template to work with correct package names

--- src/leiningen/new/solasistim.clj
+++ src/leiningen/new/solasistim.clj
@@ -1,17 +1,19 @@
-(ns leiningen.new.lein-template
+(ns leiningen.new.solasistim
   (:require [leiningen.new.templates :refer [renderer name-to-path ->files project-name multi-segment sanitize-ns]]
             [leiningen.core.main :as main]))
 
 ;; Using a nil renderer will just slurp the file.
 ;; This is questionable API design perhaps, so we conceal it here.
 
-(def expand-mustache (renderer "lein-template"))
+(def template-name "solasistim")
+
+(def expand-mustache (renderer template-name))
 (def slurp-file (fn [path]
-                  (apply (renderer "lein-template")
+                  (apply (renderer template-name)
                          [path nil])))
 
-
-(defn lein-template [name]
+;; Entry point -- Must be named the same as the template.
+(defn solasistim [name]
   (let [main-ns (multi-segment (sanitize-ns name))
         data {:raw-name name
               :name (project-name name)
@@ -20,7 +22,6 @@
               :sanitized (name-to-path name)}]
     (main/info "Generating fresh 'lein new' lein-template project.")
     (->files data
-             ["src/{{sanitized}}/foo.clj" (expand-mustache "foo.clj" data)]
              ["src/{{nested-dirs}}.clj" (expand-mustache "core.clj" data)]
              ["test/{{nested-dirs}}_test.clj" (expand-mustache "test.clj" data)]
              [".gitignore" (slurp-file "gitignore")]
